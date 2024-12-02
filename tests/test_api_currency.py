3ec4f6160c34d25c8145ae812ba0365f3d980a41
dsimanovskaya <d.simanovskaya@mirvracha.ru>
2024-12-02 16:49:54 +0300

    final api project opencart

--- tests/test_api_currency.py
+++ tests/test_api_currency.py
@@ -8,7 +8,7 @@ import pytest
                           (" ", 400), (44, 400)])
 @allure.step("Testing post_currency")
 def test_post_currency(client, currency, expected_status):
-    response = client.post_currency(currency=currency)
+    response = client.currency_api.post_currency(currency=currency)
     assert response.status_code == expected_status
     if response.status_code == 200:
         assert response.json()['success'] == 'Success: Your currency has been changed!'
